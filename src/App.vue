539c72ebaf649857a84161e5fba438c46143d2ca
Maor Elimelech - Asus <maor700@gmail.com>
2018-06-16 23:07:30 +0300

    cart view on html

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="header">
+  <h1>My Shop</h1>
+</div>
+
+<div class="my-cart-con">
+  <div class="cart-table">
+    <div class="table-header">
+      <div class="name">name</div>
+      <div class="qu">qu</div>
+      <div class="price">price</div>
+    </div>
+    <div class="cart-items">
+    <div class="item" v-for="item in cartItems" :key="item.id">
+      <span class="name">{{item.name}}</span>
+      <div class="qu">
+        <span>{{item.qu}}</span>
+        </div>
+      <span class="price">{{item.price * item.qu}}$</span>
+    </div>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const cartItems = [
+  { name: "product1", id: 1, price: 30, qu: 1 },
+  { name: "product2", id: 2, price: 20, qu: 3 },
+  { name: "product3", id: 3, price: 10, qu: 2 }
+];
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      cartItems
+    };
   }
-}
+};
 </script>
+
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Arial, Helvetica, sans-serif;
+  .price {
+    margin: 0 1em;
+  }
+  .cart-table {
+    margin: 0 auto;
+    width: 50%;
+    text-align-last: left;
+    .table-header,
+    .table-footer,
+    .cart-items > .item {
+      border: 1px solid gray;
+      padding: 0.3em;
+      display: flex;
+      .name {
+        flex: 50;
+      }
+      .price,
+      .qu {
+        flex: 25;
+      }
+    }
+
+    .cart-items > .item {
+      .qu {
+        display: flex;
+        justify-content: space-around;
+      }
+    }
+    .table-header {
+      background-color: black;
+      color: white;
+      font-weight: bold;
+    }
+  }
 }
 </style>
